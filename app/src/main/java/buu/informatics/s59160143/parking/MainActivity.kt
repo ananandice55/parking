0ad72e14415a249e3324e5e884f494132ea27e79
ananandice55 <ananandice55@gmail.com>
2019-08-21 21:37:40 +0700

    add buttonByID

--- app/src/main/java/buu/informatics/s59160143/parking/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160143/parking/MainActivity.kt
@@ -2,12 +2,19 @@ package buu.informatics.s59160143.parking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private val park: ArrayList<Parking> = ArrayList<Parking>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Button_one: Button = findViewById(R.id.button)
+        val Button_two: Button = findViewById(R.id.button2)
+        val Button_three: Button = findViewById(R.id.button3)
+
 
     }
 }
---
+++ app/src/main/java/buu/informatics/s59160143/parking/Parking.kt
@@ -0,0 +1,3 @@
+package buu.informatics.s59160143.parking
+
+data class Parking(var register: String = "", var name: String = "", var band: String = "")
